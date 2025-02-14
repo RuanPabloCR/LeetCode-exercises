48e5ba247330743d4fc104307dbfb44d13a335bd
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2025-02-14 20:26:04 +0000

    .

---
+++ Leetcode C#/TwoSum.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        // Complexidade 0(n) acredito eu.
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement)) {
+                return new int[] { map[complement], i };
+            }
+
+            map[nums[i]] = i;
+        }
+        return null;
+    }
+}
